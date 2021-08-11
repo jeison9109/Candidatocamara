3ad3351e37e5f083942e8330d8ef6e7bbcf7e836
Yeison Serna <yeisonivanserna@gmail.com>
2021-08-11 11:49:55 -0500

    versio 1.0

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCYffjE8ngph3K6yEs_1TIkIgr6t1dqgsI",
+  authDomain: "renovemoscolombia-a1c49.firebaseapp.com",
+  projectId: "renovemoscolombia-a1c49",
+  storageBucket: "renovemoscolombia-a1c49.appspot.com",
+  messagingSenderId: "913818277486",
+  appId: "1:913818277486:web:65d67efd7e46566506387f",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
